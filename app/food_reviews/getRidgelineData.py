97e7cb2b8a651c638fc3e964d39edd035bd6f8d5
davilaYuan <davilayang@gmail.com>
2019-10-29 08:07:43 +0000

    modify rigeline chart for data project

--- app/food_reviews/getRidgelineData.py
+++ app/food_reviews/getRidgelineData.py
@@ -1,70 +1,45 @@
 from app import app, db
 import pandas as pd
+import numpy as np
 
 # Process data for exporting, given start and end date
-def getRidgeline(start_date, end_date):
+def getRidgeline(start_date, end_date, n_times=6):
     query = \
     """
     SELECT 
-        main_category_en AS category, 
-        review_date, 
-        review_id AS id
+        customer_id,
+        review_id, 
+        review_date
     FROM 
         food_reviews
     WHERE 
-        energy_100g IS NOT NULL
+        verified_purchase LIKE 'Y'
         AND review_date IS NOT NULL
-        AND main_category_en IS NOT NULL
-        AND energy_100g < 3000
-        AND salt_100g < 100
-        AND main_category_en SIMILAR TO '[A-Z]_*'
         AND review_date BETWEEN '{0}' AND '{1}'
     ORDER BY
         review_date
     """.format(start_date, end_date)
     sub = pd.read_sql(query, con=db.engine)
 
-    threshold = sub.groupby('category')[['id']].count()\
-        .sort_values('id', ascending=False)\
-        .iloc[9, 0]
+    index = []
+    for times in range(1, n_times): # how many review times to show
+        for weekday in range(7): # 7 days of a week
+            index.append((times, weekday))
 
-    top10 = sub.assign(counts=lambda d: d.groupby('category')[['id']].transform('count'))\
-            .query('counts >= {}'.format(threshold))\
-            .reset_index(drop=True)
-
-    date_idx = []
-    for category in top10.category.unique():
-        for date in pd.date_range(start_date, end_date, freq='D'):
-            date_idx.append((category, date))
-
-    data = top10.groupby(['category', 'review_date'])[['id']].count()\
-        .reindex(date_idx, fill_value=0)\
-        .reset_index()\
-        .assign(byCategorySum=lambda d: d.groupby('category')[['id']].transform('sum'))\
-        .assign(p=lambda d: d.id / d.byCategorySum)\
-        .drop(['id', 'byCategorySum'], axis=1)
-
-    data = data.assign(byCategoryMaxP=lambda d: d.groupby('category')[['p']].transform(max))\
-        .assign(p_peak=lambda d: d.p / d.byCategoryMaxP)\
-        .drop(['byCategoryMaxP'], axis=1)
-
-    data = data.assign(p_lag1=lambda d: d.groupby('category')[['p_peak']].shift(-1))\
-        .assign(p_lead1=lambda d: d.groupby('category')[['p_peak']].shift(1))\
+    data = sub.assign(times=lambda df: df.groupby('customer_id')[['review_id']].transform('count'))\
+        .assign(times=lambda df: np.where(df.times >= n_times-1, n_times-1, df.times))\
+        .groupby(['times', sub.review_date.dt.dayofweek])[['review_id']].count()\
+        .reindex(index, fill_value=0)\
+        .reset_index(drop=False)\
+        .assign(byTimesSum=lambda d: d.groupby('times')[['review_id']].transform('sum'))\
+        .assign(p=lambda d: d.review_id / d.byTimesSum)\
+        .assign(byTimesMaxP=lambda d: d.groupby('times')[['p']].transform(max))\
+        .assign(p_peak=lambda d: d.p / d.byTimesMaxP)\
+        .assign(p_lag1=lambda d: d.groupby('times')[['p_peak']].shift(-1))\
+        .assign(p_lead1=lambda d: d.groupby('times')[['p_peak']].shift(1))\
         .assign(p_smooth=lambda d: (d.p_lag1 + d.p_peak + d.p_lead1) / 3)\
-        .drop(['p_lag1', 'p_lead1'], axis=1)\
-        .fillna(method='ffill', axis=1)
-
-    data = data\
-        .assign(p_lag1=lambda d: d.groupby('category')[['p_peak']].shift(-1))\
-        .assign(p_lag2=lambda d: d.groupby('category')[['p_peak']].shift(-2))\
-        .assign(p_lag3=lambda d: d.groupby('category')[['p_peak']].shift(-3))\
-        .assign(p_lead1=lambda d: d.groupby('category')[['p_peak']].shift(1))\
-        .assign(p_lead2=lambda d: d.groupby('category')[['p_peak']].shift(2))\
-        .assign(p_lead3=lambda d: d.groupby('category')[['p_peak']].shift(3))\
-        .assign(p_smooth7=lambda d: (d.p_lag1 + d.p_lag2 + d.p_lag3 + 
-                                    d.p_lead1 + d.p_lead2 + d.p_lead3 +
-                                    d.p_peak) / 7)\
-        .drop(['p_lag1', 'p_lag2', 'p_lag3', 'p_lead1', 'p_lead2', 'p_lead3'], axis=1)\
+        .drop(['review_id', 'byTimesSum', 'byTimesMaxP', 'p_lag1', 'p_lead1',], axis=1)\
+        .rename(columns={'times': 'review_times', 'review_date': 'weekday'})\
         .fillna(method='ffill', axis=1)
 
     return data.to_json(orient='records')
