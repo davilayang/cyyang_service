be45e357fad95bc54fa18bf4521a009b3ce41fd8
yang <davilayang@gmail.com>
2020-02-20 22:36:36 +0000

    create Dockerfile skeleton for db and nginx

--- manage.py
+++
@@ -1,9 +0,0 @@
-from flask_migrate import Migrate, MigrateCommand
-
-from app import app, db
-from app import models
-# make sure the models are imported by app or directly imported
-# call manage.py from root, 
-#  b.c. from app import app, db at app/modules/models.py
-
-migrate = Migrate(app, db)
