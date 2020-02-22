56d9f97e762be851b3b3c6d65aaa3d77c9049230
yang <davilayang@gmail.com>
2020-02-22 13:38:13 +0000

    add route handler for 404 error

--- services/web/app/api.py
+++ services/web/app/api.py
@@ -1,12 +1,19 @@
 # app/api.py
 # as top-level interface for flask service
 
-from dotenv import load_dotenv
+# base imports
 from os import getenv
+from dotenv import load_dotenv
+
+# flask imports
 from flask import Flask, Response
+
+# route imports
 
 app = Flask(__name__)
 
+# route handlers
+## homepage route
 @app.route("/", methods=["GET"])
 def home() -> Response:
 
@@ -23,4 +30,14 @@ def home() -> Response:
         """,
         status=200,
     )
+
+## 404 error route
+@app.errorhandler(404)
+def not_found(error, methods=["GET"]):
+
+    """
+    page not found - returns 404
+
+    """
+    return Response(f"<pre>{error}</pre> Error, Page not fonund", status=404)
 
