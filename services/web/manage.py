01c958ff1d64aed9b678217e5ebd949671de84b2
yang <davilayang@gmail.com>
2020-02-25 22:00:22 +0000

    fix error and add TODO to manage.py

--- services/web/manage.py
+++ services/web/manage.py
@@ -1,5 +1,10 @@
 # from flask_migrate import Migrate, MigrateCommand
 
+# TODO:
+# 1. instead of using Pandas, using basic modules to read and upload data
+# 2. get cli.command to work with arguments, e.g. flask seed coureswork/ flask seed skilltree
+# 3. typing, lint, docstring
+
 import json
 from pandas import read_csv
 from flask.cli import FlaskGroup
@@ -15,23 +20,44 @@ def create_db():
     db.session.commit() # commit all changes
 
 # register another command, to add new row to table
-@cli.command("seed_db") 
+@cli.command("seed_db")
 def seed_db():
     db.session.add(Coursework(category="TEST", coursename="SOME-COURSE"))
     db.session.commit()
 
+# register seed_all command to seed all tables
+@cli.command("seed_all")
+def seed_all():
+    print('seeding coursework data', flush=True)
+    insert_coursework_data()
+
+    print('seeding skilltree data', flush=True)
+    insert_skilltree_data()
+
+    print('seeding food_reviews data', flush=True)
+    insert_foodreviews_data()
+    return 
+
+
 # register seed_coursework command
 @cli.command("seed_coursework") 
 def seed_coursework():
-
     result = insert_coursework_data()
-
     return result.rowcount
 
+# register seed_skilltree command
+@cli.command("seed_skilltree") 
+def seed_skilltree():
+    result = insert_skilltree_data()
+    return result.rowcount
+
+@cli.command("seed_foodreviews") 
+def seed_foodreviews():
+    insert_foodreviews_data()
+    return 
 
 
-
-def insert_coursework_data() ->int:
+def insert_coursework_data():
 
     """
     insert_coursework_data
@@ -48,7 +74,7 @@ def insert_coursework_data() ->int:
     insertStrings = ""
     for category in data: 
         for coursename in data[category]: 
-            insertStrings += "('{0}', '{1}'), ".format(category, coursename)
+            insertStrings += f"('{category}', '{coursename}'), "
 
     # reset table
     result = db.engine.execute("DELETE FROM coursework;")
@@ -107,9 +133,7 @@ def insert_skilltree_data():
     insertStrings = ""
     for row in flattenJson(data): 
         tmp = 'NULL' if row['pid'] is None else row['pid'] # NULL for SQL
-        insertStrings += f"""
-        ({row['id']}, {tmp}, '{row['name']}', '{row['img']}', {row['size']}), 
-        """
+        insertStrings += f"({row['id']}, {tmp}, '{row['name']}', '{row['img']}', {row['size']}), "
 
     # reset table
     result = db.engine.execute("DELETE FROM skilltree;")
@@ -138,8 +162,6 @@ def insert_foodreviews_data():
         - i.e. custom functions/classes
 
     """
-    import pandas as pd
-
     dtypes = {
         'customer_id': 'object', 
         'product_parent': 'object', 
@@ -147,12 +169,14 @@ def insert_foodreviews_data():
         'helpful_votes': 'Int64', 
         'total_votes': 'Int64', 
         'code': 'object'
-    },
+    }
 
-    with open(f"{app.static_folder}/data/merged_amz-off_3.csv.gz" ) as file: 
-        data = json.load(file)
-
-    df = read_csv(filePath, compression='gzip', parse_dates='review_date')
+    df = read_csv(
+        f"{app.static_folder}/data/merged_amz-off_3.csv.gz", 
+        compression='gzip', 
+        dtype=dtypes,
+        parse_dates=['review_date']
+    )
 
     # df.review_date = pd.to_datetime(df.review_date)
     # Insert dataframe to database, as food_reviews table
